36df53a07b94ea17c62c7862017984aa293f8a9d
Andrew Moore <a.moore@lancs.ac.uk>
2016-03-15 16:20:52 +0000

    update.

--- ui/ui.py
+++ ui/ui.py
@@ -5,9 +5,13 @@ app = Flask(__name__)
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    if request.form.get('uri', None):
-        print("nionio", request.form.get['url'])
-    return render_template('hello.html', name='')
+    if request.method == 'POST':
+        return 'anything'
+    else:
+        if request.args['uri'] == 'anything':
+            return render_template('hello.html', name='')
+
+        return render_template('hello.html', name='')
 
 if __name__ == '__main__':
     app.run(debug=True)
