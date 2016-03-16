20f0996f9217a0eb30236b6590de094d4348d8b5
Andrew Moore <a.moore@lancs.ac.uk>
2016-03-16 11:23:11 +0000

    Updates to the UI.

--- ui/ui.py
+++ ui/ui.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask import request
-import search 
+import search
 import json
 import re
 
@@ -46,16 +46,28 @@ def go():
     for language, sorted_list in ranked_similarities.items():
         articleuri, sim = sorted_list[0]
         languages[language] = (articleuri, sim, search.find_article_in_list(articles, articleuri))
+    all_pictures = set()
+    for article in languages:
+        if languages[article][2] == None:
+            pass
+        else:
+            for media_items in languages[article][2]['media']['images']:
+                for images in languages[article][2]['media']['images'][media_items]:
+                    if languages[article][2]['media']['images'][media_items][images]['href'] != "http://":
+                        picture = languages[article][2]['media']['images'][media_items][images]
+                        all_pictures.add(picture['href'])
 
-    print("=> ", languages)
-    return render_template('results.html', uri = uri, articles = languages)
+    another = list(all_pictures)
+    print(type(another))
+    print(another[0])
+    return render_template('results.html', uri = uri, articles = languages, all_pictures = all_pictures, length=len(all_pictures))
 
 # Entry point
 if __name__ == '__main__':
     if not (len(sys.argv) == 3):
         print("ARGS: ARTICLE_JSON SIMILARITY_JSON")
         sys.exit(1)
-   
+
     print("Loading articles from: ", sys.argv[1]);
     f = open(sys.argv[1])
     articles = json.loads(f.read())
@@ -67,7 +79,4 @@ if __name__ == '__main__':
     f.close()
     print("Done.")
 
-    app.run(debug=True)
-
-
-
+    app.run(host='0.0.0.0')
